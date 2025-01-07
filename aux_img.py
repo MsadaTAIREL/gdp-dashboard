d5a798ef6b99a874257730d641b0096636d821d7
MsadaTAIREL <msada@taireldata.com>
2025-01-07 14:05:40 +0000

    Graficas correctas

--- aux_img.py
+++ aux_img.py
@@ -15,14 +15,13 @@ def plot_bigotes(ax, df_mean, df_min, df_max,colores = {'r':'#bf775f', 'o':'#d3a
     for d in df_mean.index:ax.plot([d]*2, [df_min.loc[d, 'RRM1'], df_max.loc[d, 'RRM1']], c = colores['b'],    marker = '_', ms = 10)
     ax.set_xlabel('Time (days)', size = 15)
     
-def img_1(sol, df0, lnp,img_file):
+def img_1(fig, ax,sol, df0, lnp,img_file):
     df_medios =    sol['df_medios_'+ lnp]
     df_min =       sol['df_min_'+ lnp]
     df_max =       sol['df_max_'+ lnp]
     df_dif =       sol['df_dif_'+ lnp]
     df_dif_min =   sol['df_dif_min_'+ lnp]
     df_dif_max =   sol['df_dif_max_'+ lnp]
-    fig, ax = plt.subplots(figsize = (8,2.5), ncols = 2)
     plot_bigotes(ax[0], df_medios, df_min, df_max, df0 = df0)
     ax[0].legend(['CT ('+lnp+')', 'SCT ('+lnp+')', 'RRM1 ('+lnp+')', 'Background'], fontsize = 7)
     ax[0].set_xticks(df_medios.index)
@@ -32,12 +31,10 @@ def img_1(sol, df0, lnp,img_file):
     ax[1].legend(['CT ('+lnp+')', 'SCT ('+lnp+')', 'RRM1 ('+lnp+')'], fontsize = 7)
     ax[1].set_title('Diferences Mean Values', size = 15)
     fig.savefig(img_file+"/"+'Valores_'+ lnp + '.png', bbox_inches='tight', pad_inches = 0)
-    return fig
 
-def img_2(sol, lnp,img_file, colores = {'r':'#bf775f', 'o':'#d3a65f',  'y':'#e6d26b','g':'#8bac86', 'c':'#98cacd','b':'#709acd', 'p':'#8176a7',  'gr': '#444444'}):
+def img_2(fig, ax,sol, lnp,img_file, colores = {'r':'#bf775f', 'o':'#d3a65f',  'y':'#e6d26b','g':'#8bac86', 'c':'#98cacd','b':'#709acd', 'p':'#8176a7',  'gr': '#444444'}):
     df_dif = sol['df_dif_'+lnp]
     df_dif_perc = pd.DataFrame({k:df_dif[k] / df_dif['SCR'] *100 for k in df_dif.columns })
-    fig, ax = plt.subplots(figsize = (8,2))
     ax.plot(df_dif_perc['CT'],   c = colores['o'],marker = '.', ms = 10)
     ax.plot(df_dif_perc['SCR'],  c = colores['r'],marker = '.', ms = 10)
     ax.plot(df_dif_perc['RRM1'], c = colores['b'],marker = '.', ms = 10)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-#from aux_img import img_1, img_2
+import seaborn as sns
+sns.set_theme()
+from aux_img import img_1, img_2
 from aux_preprocesado import preprocesado, preprocesado_columnas, bases_de_datos
-st.title("Análisis eficiencia")
-c1,c2 = st.columns(2)
+
+st.set_page_config(page_title="Análisis eficiencia", layout="wide")
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+        width: 400px;
+        margin-left: -400px;
+    }
+     
+    """,
+    unsafe_allow_html=True,
+)
+
+
+_,c1,c2,_ = st.columns([0.3, 1,2,0.3])
+
 
 with c1:
     uploaded_file = st.file_uploader("Choose a file")
@@ -17,4 +37,14 @@ with c1:
         c_met, c_lnp, c_dia, c_id = preprocesado_columnas(df)
         resumen_series, df0 = preprocesado(df)
         sol = bases_de_datos(resumen_series, df0)
+        
+        
 
+with c2:
+    if uploaded_file is not None:
+        fig, ax = plt.subplots(figsize = (8,2.5), ncols = 2)
+        img_1(fig, ax, sol, df0, 'H929',img_file)
+        st.pyplot(fig = fig)
+        fig2, ax2 = plt.subplots(figsize = (8,2.5))
+        img_2(fig2, ax2, sol, 'H929',img_file)
+        st.pyplot(fig = fig2)
