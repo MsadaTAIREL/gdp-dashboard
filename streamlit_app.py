7acf7acab83d562524b16109078eba1d3a90b2f1
MsadaTAIREL <msada@taireldata.com>
2025-01-07 14:32:51 +0000

    Añadir selección LNP

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,12 @@ import seaborn as sns
 sns.set_theme()
 from aux_img import img_1, img_2
 from aux_preprocesado import preprocesado, preprocesado_columnas, bases_de_datos
-
 st.set_page_config(page_title="Análisis eficiencia", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-     
-    """,
-    unsafe_allow_html=True,
-)
 
 
-_,c1,c2,_ = st.columns([0.3, 1,2,0.3])
+
+
+c1,c2,c3 = st.columns([ 1,2,2])
 
 
 with c1:
@@ -37,14 +24,17 @@ with c1:
         c_met, c_lnp, c_dia, c_id = preprocesado_columnas(df)
         resumen_series, df0 = preprocesado(df)
         sol = bases_de_datos(resumen_series, df0)
-        
-        
+        #
+        l_lnp = df.loc[~df[c_lnp].isna(),c_lnp].unique()
+        lnp_ef = c1.selectbox('Selecciona LNP',l_lnp)
+
 
 with c2:
-    if uploaded_file is not None:
+    if uploaded_file is not None and lnp_ef is not None:
+        c2.title('Resultados para ' + lnp_ef)
         fig, ax = plt.subplots(figsize = (8,2.5), ncols = 2)
-        img_1(fig, ax, sol, df0, 'H929',img_file)
+        img_1(fig, ax, sol, df0, lnp_ef,img_file)
         st.pyplot(fig = fig)
         fig2, ax2 = plt.subplots(figsize = (8,2.5))
-        img_2(fig2, ax2, sol, 'H929',img_file)
+        img_2(fig2, ax2, sol, lnp_ef,img_file)
         st.pyplot(fig = fig2)
